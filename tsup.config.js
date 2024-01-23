a4561d49f479356334f3b904ee87d66fd066a01f
johan <johan@baboons.se>
2024-01-23 14:12:05 +0100

    Updates target

--- tsup.config.js
+++ tsup.config.js
@@ -5,6 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: true,
+  target: "node18",
   entry: ["src/main.ts"],
   format: ["cjs", "esm"],
   minify: isProduction,
