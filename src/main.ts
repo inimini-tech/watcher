5b325a142a97c665f0cbe7c1bd32ba80062ea0e7
johan <johan@baboons.se>
2024-01-23 17:23:06 +0100

    Make exec sync

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { config } from "./config";
 import watcher from "@parcel/watcher";
 import fs from "node:fs";
 import path from "path";
-import { exec } from "child_process";
+import { execSync } from "child_process";
 
 async function main() {
   console.log("Starting watcher");
@@ -15,20 +15,11 @@ async function main() {
         if (stat.isFile()) {
           console.log("New file detected, ", event.path);
 
-          exec(
+          const result = execSync(
             `open -a ${config.GARMENT_FILTER_APP.replace(/(\s+)/g, "\\$1")} ${event.path.replace(/(\s+)/g, "\\$1")}`,
-            (error, stdout, stderr) => {
-              if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-              }
-              if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-              }
-              console.log(`stdout: ${stdout}`);
-            },
           );
+
+          console.log(result);
 
           //const newPath = `${config.GARMENT_OUT_PATH}/${path.posix.basename(event.path)}`;
           //fs.renameSync(event.path, newPath);
