10deb4cab3307d2464bbcb030067a6730640987c
johan <johan@baboons.se>
2024-01-24 08:55:47 +0100

    buggfix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function main() {
   const subscription = await watcher.subscribe(
     config.GARMENT_WATCH_PATH,
     async (err, events) => {
-      console.log("new event detected", event);
+      console.log("new event detected", events);
       events.forEach(async (event) => {
         const stat = fs.lstatSync(event.path);
 
