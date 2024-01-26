eea97eb8c8cce4517b2c5a8cc3e91893cdc7ed16
johan <johan@baboons.se>
2024-01-26 10:49:05 +0100

    Path fix

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const config = {
     "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/hotfolder",
   GARMENT_PS_WATCH_PATH: "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_PNG",
   GARMENT_FILTER_APP:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/_enstakaplagg_macPro.app",
+    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_enstakaplagg_macPro.app",
   GARMENT_OUT_PATH:
     "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_OREDIGERADE",
 };
