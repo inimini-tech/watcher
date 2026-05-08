b3e3ba1066c7393443abf584b390dc0e9dacdbb2
johan <johan@baboons.se>
2026-05-08 08:34:24 +0200

    fix: updated paths

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 export const config = {
   GARMENT_WATCH_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/hotfolder",
-  GARMENT_PS_WATCH_PATH: "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_PNG",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/hotfolder",
+  GARMENT_PS_WATCH_PATH: "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_PNG",
   GARMENT_PS_PROCESS_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_PS",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_PS",
   GARMENT_FILTER_APP:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_enstakaplagg_macPro.app",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_enstakaplagg_macPro.app",
   GARMENT_OUT_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_OREDIGERADE",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_OREDIGERADE",
   GARMENT_COMPLETED_OUT_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_UPPLADDADE",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_UPPLADDADE",
   AGENTS_WATCH_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/hotfolder-agents",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/hotfolder-agents",
   AGENTS_UPSCALE_OUT_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/hotfolder-upscale",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/hotfolder-upscale",
   AGENTS_PROCESSING_PATH:
-    "/Users/inimini/Dropbox/MINIKIT PHOTO/INIMINI EP/_AGENTS_PROCESSING",
+    "/Users/inimini/Inimini Dropbox/Mini Kit/MINIKIT PHOTO/INIMINI EP/_AGENTS_PROCESSING",
 };
